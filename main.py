556db12bd5c738301676e3d8dff5599c00a89a97
EhsanTgv <EhsanTgv@gmail.com>
2021-11-22 15:57:22 +0330

    Styling Terminal

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import curses
 from curses import wrapper
 
 
 def main(stdscr):
+    curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     stdscr.clear()
-    stdscr.addstr("Hello World!")
+    stdscr.addstr(0, 0, "Hello World!")
     stdscr.refresh()
     stdscr.getkey()
 
