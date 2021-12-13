b0024448b247e359acfd9e97d5b8ffd676840e48
EhsanTgv <EhsanTgv@gmail.com>
2021-12-13 16:57:16 +0300

    Calculating WPM

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import curses
+import time
 from curses import wrapper
 
 
@@ -12,6 +13,7 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"wpm: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
@@ -24,13 +26,22 @@ def display_text(stdscr, target, current, wpm=0):
 def wpm_test(stdscr):
     target_text = "Hello world this is some test text for this pop!"
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear()
-        display_text(stdscr, target_text, current_text)
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
         if ord(key) == 27:
             break
